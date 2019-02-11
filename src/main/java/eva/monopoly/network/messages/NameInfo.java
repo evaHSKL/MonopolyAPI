c39b21adfa186616634a0ddd950c379ae25b2b26
Wolf2323 <gabrielpatrikurban@gmail.com>
2019-02-11 15:13:17 +0100

    added PlayerStatusChanged Message

--- src/main/java/eva/monopoly/network/messages/NameInfo.java
+++ src/main/java/eva/monopoly/network/messages/NameInfo.java
@@ -4,13 +4,13 @@ import eva.monopoly.network.api.ExchangeMessage;
 
 public class NameInfo extends ExchangeMessage {
 	private static final long serialVersionUID = 3601976405972934497L;
-	String name;
-
-	public String getName() {
-		return name;
-	}
+	private final String name;
 
 	public NameInfo(String name) {
 		this.name = name;
+	}
+
+	public String getName() {
+		return name;
 	}
 }
---
+++ src/main/java/eva/monopoly/network/messages/PlayerStatusChanged.java
@@ -0,0 +1,25 @@
+package eva.monopoly.network.messages;
+
+import eva.monopoly.network.api.ExchangeMessage;
+
+public class PlayerStatusChanged extends ExchangeMessage {
+
+	private static final long serialVersionUID = 1L;
+	private final ConnectionState state;
+
+	public PlayerStatusChanged(ConnectionState state) {
+		this.state = state;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public ConnectionState getState() {
+		return state;
+	}
+
+	public static enum ConnectionState {
+		CONNECTED, DISCONNECTED, RECONNECTED;
+	}
+}

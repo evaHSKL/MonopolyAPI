ae5c167b1ee5148ee25e3ff3f8735b374f4f49e9
Wolf2323 <gabrielpatrikurban@gmail.com>
2019-02-13 13:50:20 +0100

    PlayerStatusChanged added Name

--- src/main/java/eva/monopoly/api/network/messages/PlayerStatusChanged.java
+++ src/main/java/eva/monopoly/api/network/messages/PlayerStatusChanged.java
@@ -5,14 +5,25 @@ import eva.monopoly.api.network.api.ExchangeMessage;
 public class PlayerStatusChanged extends ExchangeMessage {
 
 	private static final long serialVersionUID = 1L;
+	private final String name;
 	private final ConnectionState state;
 
 	public PlayerStatusChanged(ConnectionState state) {
+		this.name = null;
+		this.state = state;
+	}
+
+	public PlayerStatusChanged(String name, ConnectionState state) {
+		this.name = name;
 		this.state = state;
 	}
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
+	}
+
+	public String getName() {
+		return name;
 	}
 
 	public ConnectionState getState() {

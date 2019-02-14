8f6d6eae9a544e3e56643624affb0602fdb00e13
Wolf2323 <gabrielpatrikurban@gmail.com>
2019-02-14 16:17:37 +0100

    name attribute added to ExchangeMessage

--- src/main/java/eva/monopoly/api/network/api/ExchangeMessage.java
+++ src/main/java/eva/monopoly/api/network/api/ExchangeMessage.java
@@ -3,6 +3,19 @@ package eva.monopoly.api.network.api;
 import java.io.Serializable;
 
 public abstract class ExchangeMessage implements Serializable {
+	private final String name;
+
+	public ExchangeMessage(String name) {
+		this.name = name;
+	}
+
+	public ExchangeMessage() {
+		this.name = null;
+	}
+
+	public String getName() {
+		return name;
+	}
 
 	private static final long serialVersionUID = -2894938167336591768L;
 
--- src/main/java/eva/monopoly/api/network/messages/PlayerStatusChanged.java
+++ src/main/java/eva/monopoly/api/network/messages/PlayerStatusChanged.java
@@ -5,25 +5,20 @@ import eva.monopoly.api.network.api.ExchangeMessage;
 public class PlayerStatusChanged extends ExchangeMessage {
 
 	private static final long serialVersionUID = 1L;
-	private final String name;
 	private final ConnectionState state;
 
 	public PlayerStatusChanged(ConnectionState state) {
-		this.name = null;
+		super();
 		this.state = state;
 	}
 
 	public PlayerStatusChanged(String name, ConnectionState state) {
-		this.name = name;
+		super(name);
 		this.state = state;
 	}
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
-	}
-
-	public String getName() {
-		return name;
 	}
 
 	public ConnectionState getState() {

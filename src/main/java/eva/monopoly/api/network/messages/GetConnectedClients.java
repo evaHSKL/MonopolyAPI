51089c5e062dc80ceca0e3b3a53063cf1164eeec
Philipp Spandl <ph.spandl@gmx.de>
2019-02-16 18:53:09 +0100

    added message GetConnectedClients

---
+++ src/main/java/eva/monopoly/api/network/messages/GetConnectedClients.java
@@ -0,0 +1,14 @@
+package eva.monopoly.api.network.messages;
+
+import eva.monopoly.api.network.api.ExchangeMessage;
+
+public class GetConnectedClients extends ExchangeMessage {
+
+	private static final long serialVersionUID = 5455503451578318408L;
+
+	public GetConnectedClients(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}

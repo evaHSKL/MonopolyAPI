fb92e1eb6ba4e3e7000c824084c12af3589cdccc
Philipp Spandl <ph.spandl@gmx.de>
2019-02-16 15:10:20 +0100

    finalised ReadyStatusChanged

--- src/main/java/eva/monopoly/api/network/messages/ReadyStatusChanged.java
+++ src/main/java/eva/monopoly/api/network/messages/ReadyStatusChanged.java
@@ -3,7 +3,14 @@ package eva.monopoly.api.network.messages;
 import eva.monopoly.api.network.api.ExchangeMessage;
 
 public class ReadyStatusChanged extends ExchangeMessage {
+
+	private static final long serialVersionUID = -2752293858671047996L;
+
 	public ReadyStatusChanged() {
 		super();
+	}
+
+	public ReadyStatusChanged(String name) {
+		super(name);
 	}
 }

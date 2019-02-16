5c9ff37ec2f90e728c19a28e25f1e63c9db80d4a
Philipp Spandl <ph.spandl@gmx.de>
2019-02-16 15:06:03 +0100

    ReadyStatusChanged added

---
+++ src/main/java/eva/monopoly/api/network/messages/ReadyStatusChanged.java
@@ -0,0 +1,9 @@
+package eva.monopoly.api.network.messages;
+
+import eva.monopoly.api.network.api.ExchangeMessage;
+
+public class ReadyStatusChanged extends ExchangeMessage {
+	public ReadyStatusChanged() {
+		super();
+	}
+}

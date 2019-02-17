feb3eb5c2fedeaf5436d19d815fe7bfe08c8505e
Wolf2323 <gabrielpatrikurban@gmail.com>
2019-02-17 17:19:52 +0100

    added buyable option

--- src/main/java/eva/monopoly/api/game/card/cards/MoveTargetCard.java
+++ src/main/java/eva/monopoly/api/game/card/cards/MoveTargetCard.java
@@ -5,11 +5,13 @@ import eva.monopoly.api.game.card.Card;
 public abstract class MoveTargetCard extends Card {
 	protected final String target;
 	protected final int moneyModyfire;
+	protected final boolean buyable;
 
-	public MoveTargetCard(String text, CardType type, String target, int moneyModyfire) {
+	public MoveTargetCard(String text, CardType type, String target, int moneyModyfire, boolean buyable) {
 		super(text, type);
 		this.target = target;
 		this.moneyModyfire = moneyModyfire;
+		this.buyable = buyable;
 	}
 
 }

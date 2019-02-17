3766cc1c04a54a2508d86342d564105e1ed77f5b
Wolf2323 <gabrielpatrikurban@gmail.com>
2019-02-17 23:53:06 +0100

    added getter

--- src/main/java/eva/monopoly/api/game/street/streets/NonBuyableMoneyStreet.java
+++ src/main/java/eva/monopoly/api/game/street/streets/NonBuyableMoneyStreet.java
@@ -5,10 +5,14 @@ import eva.monopoly.api.game.street.NonBuyableStreet;
 public abstract class NonBuyableMoneyStreet extends NonBuyableStreet {
 	private static final long serialVersionUID = 296566574260469073L;
 
-	protected int amount;
+	protected final int amount;
 
 	public NonBuyableMoneyStreet(String name, int amount) {
 		super(name);
 		this.amount = amount;
+	}
+
+	public int getAmount() {
+		return amount;
 	}
 }

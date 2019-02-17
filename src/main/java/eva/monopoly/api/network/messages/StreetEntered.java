5f3a65d05051947eb135d93a3ce3ed1879c4cf30
Wolf2323 <gabrielpatrikurban@gmail.com>
2019-02-17 17:51:44 +0100

    fix card changed to street

--- src/main/java/eva/monopoly/api/network/messages/StreetEntered.java
+++ src/main/java/eva/monopoly/api/network/messages/StreetEntered.java
@@ -2,25 +2,25 @@ package eva.monopoly.api.network.messages;
 
 import java.util.OptionalInt;
 
-import eva.monopoly.api.game.card.Card;
+import eva.monopoly.api.game.street.Street;
 import eva.monopoly.api.network.api.ExchangeMessage;
 
 public class StreetEntered extends ExchangeMessage {
 	private static final long serialVersionUID = 5726050951454441818L;
 
-	private final Card card;
+	private final Street street;
 	private final OptionalInt moneyAmount;
 	private final int newMoney;
 
-	public StreetEntered(String name, Card card, OptionalInt moneyAmount, int newMoney) {
+	public StreetEntered(String name, Street street, OptionalInt moneyAmount, int newMoney) {
 		super(name);
-		this.card = card;
+		this.street = street;
 		this.moneyAmount = moneyAmount;
 		this.newMoney = newMoney;
 	}
 
-	public Card getCard() {
-		return card;
+	public Street getStreet() {
+		return street;
 	}
 
 	public OptionalInt getMoneyAmount() {

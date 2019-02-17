1521b2d7e7ea4186b84fe9330fe433f7625b9436
Wolf2323 <gabrielpatrikurban@gmail.com>
2019-02-17 14:32:50 +0100

    added doublets to RollDice

--- src/main/java/eva/monopoly/api/network/messages/RollDice.java
+++ src/main/java/eva/monopoly/api/network/messages/RollDice.java
@@ -6,18 +6,25 @@ public class RollDice extends ExchangeMessage {
 	private static final long serialVersionUID = -4546520837831237947L;
 
 	private final int amount;
+	private final boolean doublets;
 
 	public RollDice(String name) {
 		super(name);
 		this.amount = 0;
+		this.doublets = false;
 	}
 
-	public RollDice(String name, int amount) {
+	public RollDice(String name, int amount, boolean doublets) {
 		super(name);
 		this.amount = amount;
+		this.doublets = doublets;
 	}
 
 	public int getAmount() {
 		return amount;
+	}
+
+	public boolean isDoublets() {
+		return doublets;
 	}
 }

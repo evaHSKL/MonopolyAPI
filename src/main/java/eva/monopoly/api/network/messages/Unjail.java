e49a8c95280e7fa5ce2cbf181be773fd1dc2d75e
Wolf2323 <gabrielpatrikurban@gmail.com>
2019-02-17 15:19:39 +0100

    added Unjail

---
+++ src/main/java/eva/monopoly/api/network/messages/Unjail.java
@@ -0,0 +1,22 @@
+package eva.monopoly.api.network.messages;
+
+import eva.monopoly.api.network.api.ExchangeMessage;
+
+public class Unjail extends ExchangeMessage {
+	private static final long serialVersionUID = -7488473280417125732L;
+
+	private final UnjailReason reason;
+
+	public Unjail(String name, UnjailReason reason) {
+		super(name);
+		this.reason = reason;
+	}
+
+	public UnjailReason getReason() {
+		return reason;
+	}
+
+	public static enum UnjailReason {
+		PAYED, CARD, DOUBLETS;
+	}
+}

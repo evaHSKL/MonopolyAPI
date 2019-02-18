c1bec572fd40ff914e11ae66d7307482d21d185c
Wolf2323 <gabrielpatrikurban@gmail.com>
2019-02-18 05:07:04 +0100

    fix type

--- src/main/java/eva/monopoly/api/network/messages/GetStreets.java
+++ src/main/java/eva/monopoly/api/network/messages/GetStreets.java
@@ -2,24 +2,25 @@ package eva.monopoly.api.network.messages;
 
 import java.util.List;
 
+import eva.monopoly.api.game.street.BuyableStreet;
 import eva.monopoly.api.network.api.ExchangeMessage;
 
 public class GetStreets extends ExchangeMessage {
 	private static final long serialVersionUID = -1303534427156165466L;
 
-	private final List<BuyStreet> streets;
+	private final List<BuyableStreet> streets;
 
 	public GetStreets(String name) {
 		super(name);
 		this.streets = null;
 	}
 
-	public GetStreets(String name, List<BuyStreet> streets) {
+	public GetStreets(String name, List<BuyableStreet> streets) {
 		super(name);
 		this.streets = streets;
 	}
 
-	public List<BuyStreet> getStreets() {
+	public List<BuyableStreet> getStreets() {
 		return streets;
 	}
 }

a0e3587cc0c8e60c78b9194a117e31175ffc9f94
Wolf2323 <gabrielpatrikurban@gmail.com>
2019-02-18 04:56:21 +0100

    changed messages

--- src/main/java/eva/monopoly/api/network/messages/StartStopRound.java
+++ src/main/java/eva/monopoly/api/network/messages/StartStopRound.java
@@ -1,31 +1,25 @@
 package eva.monopoly.api.network.messages;
 
+import eva.monopoly.api.game.player.Player;
 import eva.monopoly.api.network.api.ExchangeMessage;
 
 public class StartStopRound extends ExchangeMessage {
 	private static final long serialVersionUID = 147328409676047602L;
 
-	private final int newMoney;
-	private final int newPosition;
+	private final Player player;
 
 	public StartStopRound(String name) {
 		super(name);
-		this.newMoney = -1;
-		this.newPosition = -1;
+		this.player = null;
 	}
 
-	public StartStopRound(String name, int newMoney, int newPosition) {
+	public StartStopRound(String name, Player player) {
 		super(name);
-		this.newMoney = newMoney;
-		this.newPosition = newPosition;
+		this.player = player;
 	}
 
-	public int getNewMoney() {
-		return newMoney;
-	}
-
-	public int getNewPosition() {
-		return newPosition;
+	public Player getPlayer() {
+		return player;
 	}
 
 }

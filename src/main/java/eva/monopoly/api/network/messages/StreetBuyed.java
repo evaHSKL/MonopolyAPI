8e5ab0ef5faaab635c2f74179d36261601e40c13
Wolf2323 <gabrielpatrikurban@gmail.com>
2019-02-18 04:07:13 +0100

    added newMoney amount to StreetBuyed

--- src/main/java/eva/monopoly/api/network/messages/StreetBuyed.java
+++ src/main/java/eva/monopoly/api/network/messages/StreetBuyed.java
@@ -7,13 +7,20 @@ public class StreetBuyed extends ExchangeMessage {
 	private static final long serialVersionUID = 550164991138143917L;
 
 	private final BuyableStreet street;
+	private final int newMoney;
 
-	public StreetBuyed(String name, BuyableStreet street) {
+	public StreetBuyed(String name, BuyableStreet street, int newMoney) {
 		super(name);
 		this.street = street;
+		this.newMoney = newMoney;
 	}
 
 	public BuyableStreet getStreet() {
 		return street;
 	}
+
+	public int getNewMoney() {
+		return newMoney;
+	}
+
 }

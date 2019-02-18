3602099a443c6878dea7d435d0ffd15ffd4004ab
Wolf2323 <gabrielpatrikurban@gmail.com>
2019-02-18 04:33:37 +0100

    added Money and Position Attribute

--- src/main/java/eva/monopoly/api/network/messages/StartStopRound.java
+++ src/main/java/eva/monopoly/api/network/messages/StartStopRound.java
@@ -5,8 +5,21 @@ import eva.monopoly.api.network.api.ExchangeMessage;
 public class StartStopRound extends ExchangeMessage {
 	private static final long serialVersionUID = 147328409676047602L;
 
-	public StartStopRound(String name) {
+	private final int newMoney;
+	private final int newPosition;
+
+	public StartStopRound(String name, int newMoney, int newPosition) {
 		super(name);
+		this.newMoney = newMoney;
+		this.newPosition = newPosition;
+	}
+
+	public int getNewMoney() {
+		return newMoney;
+	}
+
+	public int getNewPosition() {
+		return newPosition;
 	}
 
 }
